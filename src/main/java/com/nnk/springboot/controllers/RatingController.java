cda537ec3b142f7718ede7afb0f228d809fe560f
N-Red <63408242+N-Red@users.noreply.github.com>
2021-10-05 09:21:59 +0200

    add CRUD Rating

--- src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -22,6 +22,7 @@ public class RatingController {
     public String home(Model model)
     {
         // TODO: find all Rating, add to model
+        model.addAttribute("ratingList",ratingService.findAll());
         return "rating/list";
     }
 
@@ -32,26 +33,35 @@ public class RatingController {
 
     @PostMapping("/rating/validate")
     public String validate(@Valid Rating rating, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Rating list
+        if(!result.hasErrors()){
+            ratingService.save(rating);
+            model.addAttribute("ratingList",ratingService.findAll());
+            return "redirect:/rating/list";
+        }
         return "rating/add";
     }
 
     @GetMapping("/rating/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Rating by Id and to model then show to the form
+        model.addAttribute("rating",ratingService.updateById(id));
         return "rating/update";
     }
 
     @PostMapping("/rating/update/{id}")
     public String updateRating(@PathVariable("id") Integer id, @Valid Rating rating,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Rating and return Rating list
+        if (result.hasErrors()){
+            return "rating/list";
+        }
+        ratingService.updateByRating(rating,id);
+        model.addAttribute("ratingList",ratingService.findAll());
         return "redirect:/rating/list";
     }
 
     @GetMapping("/rating/delete/{id}")
     public String deleteRating(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Rating by Id and delete the Rating, return to Rating list
+        ratingService.delete(id);
+        model.addAttribute("ratingList",ratingService.findAll());
         return "redirect:/rating/list";
     }
 }
--- src/main/java/com/nnk/springboot/services/RatingService.java
+++ src/main/java/com/nnk/springboot/services/RatingService.java
@@ -1,11 +1,41 @@
 package com.nnk.springboot.services;
 
+import com.nnk.springboot.domain.Rating;
 import com.nnk.springboot.repositories.RatingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RatingService {
     @Autowired
     private RatingRepository ratingRepository;
+
+    public List<Rating> findAll() {
+        return ratingRepository.findAll();
+    }
+
+    public void save(Rating rating) {
+        ratingRepository.save(rating);
+    }
+
+    public Rating updateById(Integer id) {
+        return this.findById(id);
+    }
+
+    public void updateByRating(Rating rating, Integer id) {
+        rating.setId(id);
+        ratingRepository.save(rating);
+    }
+
+    private Rating findById(Integer id) {
+        return ratingRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Rating Id:" + id));
+    }
+
+    public void delete(Integer id) {
+        Rating rating = this.findById(id);
+        ratingRepository.delete(rating);
+    }
 }
